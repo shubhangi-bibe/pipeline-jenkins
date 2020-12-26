563016118e9948f3cc7409a8d19bba6123794f08
Shubhangi Bibe <shubhangibibe@gmail.com>
2020-12-26 14:10:14 +0530

    fisrt commi

---
+++ hello-world-jenkins/src/main/java/com/cg/training/hello/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.cg.training.hello;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("Hello java");
+	}
+
+}
